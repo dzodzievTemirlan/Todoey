6dcb6950cf20a177a1f946b2f3c165f4ac7336bf
dzodzievTemirlan <uccm.timmy@gmail.com>
2020-04-08 11:36:32 +0300

    Todo App

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Temirlan Dzodziev on 29.03.2020.
+//  Copyright © 2020 Temirlan Dzodziev. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Temirlan Dzodziev on 29.03.2020.
+//  Copyright © 2020 Temirlan Dzodziev. All rights reserved.
+//
+
+import Foundation
